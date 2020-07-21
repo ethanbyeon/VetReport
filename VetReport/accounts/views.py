e6926bb543b8ddc8fd05ec277b2e27221ea6f870
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-07-20 23:59:24 -0700

    added dashboard

--- VetReport/accounts/views.py
+++ VetReport/accounts/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home/index.html')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
