68c4f012975cde3ea649630ee10e6ccc26fefea4
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-07-30 22:05:32 -0700

    got rid of register  page

--- VetReport/accounts/urls.py
+++ VetReport/accounts/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
 
     path('dashboard/', views.dashboard, name='dashboard'),
 
--- VetReport/accounts/views.py
+++ VetReport/accounts/views.py
@@ -9,8 +9,5 @@ def home(request):
 def login(request):
     return render(request, 'accounts/login.html')
 
-def register(request):
-    return render(request, 'accounts/register.html')
-
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
