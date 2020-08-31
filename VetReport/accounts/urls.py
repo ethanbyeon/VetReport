bd5affa1beec7480083981d58701c801f601380b
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-08-30 20:11:12 -0700

    finished case table

--- VetReport/accounts/urls.py
+++ VetReport/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('accounts/login/', views.login, name='login'),
     
     path('user/', views.userPage, name='user_page'),
-    path('create_case/<int:pk>/', views.createCase, name='create_case'),
+    path('create_case/', views.createCase, name='create_case'),
     path('update_case/<int:pk>/', views.updateCase, name='update_case'),
     path('delete_case/<int:pk>/', views.deleteCase, name='delete_case'),
 
